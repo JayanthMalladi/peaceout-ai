d6432db16298ab9eabfa6ed55ba2042da1971443
JayanthMalladi <jayanthmalladi1@gmail.com>
2024-12-26 12:44:46 +0530

    improved functionality of the website

--- app/lib/mira-client.ts
+++ app/lib/mira-client.ts
@@ -1,5 +1,5 @@
 const MIRA_API_URL = 'https://flow-api.mira.network/v1/flows/flows/jaymalladi/mood-analysis-chatbot'
-const FLOW_VERSION = "1.0.6"
+const FLOW_VERSION = "1.0.7"
 
 export async function getMiraResponse(input: string) {
   const apiKey = process.env.MIRA_API_KEY
